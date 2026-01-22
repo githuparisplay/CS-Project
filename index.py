a2d34c0ee27829b4e899142ca2453004ca7fe1af
Paris <parispowell14atwork@gmail.com>
2026-01-22 17:54:48 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,302 @@
+# MISSION SCHOOL
+
+# Scene 1 : Intro
+
+#Your species is trying to set up a living courters on Plaent Earth.
+#As a Mission Specialist, your next assignment to inhabit Earth is to find School!
+#Find School suitable to your Species, and send the report. You only need to attend school for a day to complete the report.
+
+#Description
+    #Welcome Space Agent, to your new mission on Planet Earth.
+
+#What is your mission Space Agent?
+
+#Choices
+    #Input "Mission 1" for MISSION SCHOOL.
+    #Input "Mission 2" for MISSION SABATAGE.
+
+#Outcomes
+    #Mission 1: Your ready to depart to Earth for MISSION SCHOOL.
+    #Mission 2: You believe that your species is the Villian and you are the Hero. Be ready to save the Humans, or die alone.
+    
+#Consequences
+    #Mission 1: You have selected MISSION SCHOOL. Your species has given you items to help you on your mission. (You begin with NARRATOR)
+    #Mission 2: You have selected MISSION SABATAGE. Your species is now a enemy. You have not been given items to help you on your mission. (You begin with INNOR THOUGHTS)
+
+#NARRATOR: Today marks the begining of a new chapter for your species. You have been chosen to be the first of your kind to set foot on Planet Earth, School. Your mission is clear: to find and attend School, a crucial institution for the Species: Human, and report back your findings. (You proceed to Scene 1 : Begin Mission SCHOOL (Report Arc)
+#INNOR THOUGHTS: Your species has always been the silent observers, watching from the shadows as the Species: Human, go about their lives. But no more. Today, you embark on a mission to infiltrate your species ways, to uncover their plants, and to ultimately sabotage their efforts to colonize other planets. Your first target: School. (You proceed to Scene 1 : Begin Mission SCHOOL (Report Arc)
+
+#Careful, Pleant Earth is still unkown.
+
+
+#Scene 1 : Begin Mission SCHOOL (Hero Arc)
+#QUARTERS
+
+#Description
+    #It's time to embark Hero!
+    #Today's assignment is MISSION ATTEND SCHOOL.
+    #You have crafted items to keep you safe.
+    #To Check Inventory input, "Inventory", at any time.
+    #You have these items: 10 oxygen fillters, 6 deadly rubber bands.
+    #Your HP is full. Check HP anytime while viewing Inventory.
+
+#Where do you want to go?
+
+#Choices
+    #A) I would like to go to School.
+    #B) I would like to explore more of Planet Earth first.
+    #C) I would like to check out my Quarters.
+    #D) I would like to end my mission here.
+    #E) I would like to read my mission briefing again.
+#Notice: You can return to these choices by inputting, "Quarters" during Description. Some choices may be unavailable or changed based on your Inventory or HP.
+#Notice: You will not move on with your mission until you choose A) to go to School.
+
+#Outcomes
+#A) You have arrived at School. Time to begin your mission.
+#B) You have explored more of Planet Earth. You found a map.
+#C) You have checked out your Quarters. Your Quarters as additional funttions: Craft, Heal, Lookout.
+#D) You have ended your mission here. Your species is confused at your choice.
+#E) (Mission Briefing)
+#Notice: You do not loose any oxygen fillters while in your Quarters, unless your choice leads you to Earth.
+
+#Consequences
+#A) You are ready to begin MISSION ATTEND SCHOOL.  (You proceed to Scene 1 : Begin Mission SCHOOL (of Hero Arc)
+#B) New item is in your Inventory: Map. (-1 oxygen fillter)
+#C) You can use any of these functions while in your Quarters by inputting their name.
+#D) Game Over!  (Returned to Scene 1 : Intro.)
+#E) You have read your mission briefing again. You feel more prepared for your mission.
+
+
+#COMING SCOON: MISSION SABATAGE
+
+
+#Scene 1 : Begin Mission SCHOOL (Report Arc)
+#QUARTERS
+
+#Description
+    #It's time for school Space Agent!
+    #Today's assignment is MISSION ATTEND SCHOOL.
+    #You've been given items to keep.
+    #To Check Inventory input, "Inventory", at any time.
+    #You have these items: 5 oxygen fillters, 2 water balloons.
+    #Your HP is full. Check HP anytime while viewing Inventory.
+
+#Where do you want to go?
+
+#Choices
+    #A) I would like to go to School.
+    #B) I would like to contact my species.
+    #C) I would like to check out my Quarters.
+    #D) I would like to end my mission here.
+    #E) I would like to read my mission briefing again.
+#Notice: You can return to theses Choices by inputting, "Quarters" at any time during Description. Some choices may be unavailable or changed based on your Inventory or HP.
+#Notice: You will not move on with your mission until you choose A) to go to School.
+
+#Outcomes
+#A) You have arrived at School. Time to begin your mission.
+#B) You have contacted your species. They remind you of your mission.
+#C) You have checked out your Quarters. Your Quarters as additional funttions: Craft, Heal, Lookout.
+#D) You have ended your mission here. Your species is confused at your choice.
+#E) (Mission Briefing)
+#Notice: You do not loose any oxygen fillters while in your Quarters, unless your choice leads you to Earth.
+
+#Consequences
+#A) You are ready to begin MISSION ATTEND SCHOOL.  (You proceed to Scene 1 : Begin Mission SCHOOL (of Report Arc)
+#B) Your species encourages you to get started on your mission.
+#C) You can use any of these functions while in your Quarters by inputting their name.
+#D) Game Over!  (Returned to Scene 1 : Intro.)
+#E) You have read your mission briefing again. You feel more prepared for your mission.
+
+#Scene 1 : Begin Mission SCHOOL
+
+#You've been given items.
+#Which item should you begin with?
+
+#Choices
+    #A) Computer with stolen login details.
+    #B) A Uniform with unfamilar writings weaved into it.
+    #C) Mysterious Unlimited Credit Card.
+
+#Outcomes
+    #A) The login details were correct! You are now a student at Online Academy.
+    #B) Earth Humans won't notice you are not one of them if you wear Unifrom wtih unfamilar writings. You are now a student at InPersonal School.
+    #C) Your species does not know enough to be able to use Mysterious Unlimited Credit Card.
+#Notice: Your Spieces Know nothing about Plaent Earth except that it exist and species: Human, have School. Your spieces must have school inorder to live on Earth.
+
+
+#Consequences
+    #A) You complete MISSION ATTEND SCHOOL and are now a student at Online Academy. (-1 oxygen fillters)
+    #B) You complete MISSION ATTEND SCHOOL and are now a student at InPersonal School. (-1 oxygen fillters)
+    #C) You've failed MISSION ATTEND SCHOOL. You've wasted oxygen. (-1 oxygen fillters)   Returned to Outcomes with -1 oxygen filter in inventory, decreased HP, and only A) amd B) choices remaining.
+#Notice: If all oxygen fillters are exhusted, your HP will rapidly decrease, soon, not able to move and be left to species: Human, doctors for reserach. Game Over!
+
+#Scene 2 : School Begins
+
+#You wrote your findings: I have sucessful found School. I must now enter School to continue my report.
+
+#Description: Computer with stolen login details.
+    #Today's assignment is MISSION SHOW UP.
+    #New function has been sent from your species! Input, "Student Details", to see who you are.
+    #You have not been given any items.
+
+#How should you show up to class?
+
+#Choices
+    #A) Click Join on any of the classes.
+    #B) Wait to be called into a class.
+
+#Outcomes
+    #A) You began your own meeting under Class: Chemistry. Nobody joined your room and you spent the whole day staring at yourself on video.
+    #B) Species: Human, teacher noticed you were not in a meeting and invited you to Class: Morning Meeting.
+
+#Consequences
+    #A) Game Over! Species: Human, found your body. Unable to move you were used in their research. (loose all oxygen fillters)
+    #B) You completed MISSION SHOW UP and have been given a schedule. (-1 oxygen fillters.) New item is in your Inventory: Schedule)
+
+#Scene 3 : Class
+
+#You wrote your findings: I have sucessful entered School. I must now attend species: Human, class to continue my report.
+
+#Description: Computer with stolen login details.
+    #Today's assignment is MISSION CLASS.
+    #You've joined the correct class using, schedule.
+    #You have not been given any items.
+
+#Species: Human, teacher asks you to turn your camera on.
+
+#Choices
+    #A) Turn on camera.
+    #B) Do not turn on camera.
+
+#Outcomes
+    #A) Species: Human, teacher sees you are not Human. You try to explain your mission to Species Human, teacher, but the teacher cannot understand you. You are reported to the authorites and kicked out the class.
+    #B) Species: Human, teacher reports you to school admin, your kicked out the class and cannot join school.
+
+#Consequences
+    #A) Game Over! Species: Human Authorites, tracks your location from the report. You were used in their research. You've failed MISSION CLASS.
+    #B) Game Over!You used all your oxygen and gather no infromation about School. You've failed MISSION ClASS. (loose all oxygen fillters)
+
+#Scene 2: School Begins
+
+#You wrote your findings: I have sucessful found School. I must now enter School to continue my report.
+
+#Description: A Unifrom with unfamilar writings weaved into it.
+    #Today's assignment is MISSION SHOW UP.
+    #New function has been sent from your species! Input, "Student Details", to see who you are.    
+    #You have not been given any items.
+
+#How should you show up to class?
+
+#Choices
+    #A) Infultrate the building and be to class on time.
+    #B) Wait and walk into school with the Species: Little Humans.
+
+#Outcomes
+    #A) Species: Human, security guard saw you siting, staring, in the dark of a classroom.
+    #B) Your just their size and sucessfully infultrate school. (-1 oxygen fillters)
+
+#Consequences
+    #A) Your redirected outdoors and are forced to wait hours outside till school begins. (-1 oxygen, decreased HP)
+    #B) You complete MISSION SHOW UP. The Species: Little Humans, are all given snacks. (increased HP, if HP is already full then +1 oxygen fillter)
+
+#Scene 3 : POISON
+
+#You wrote your findings: I have sucessful entered School. I must now attend Species: Human, class to continue my report.
+
+#Description: A Unifrom with unfamilar writings weaved into it.
+    #Today's assignment is MISSION CLASS.
+    #New items are in your Inventory: 1 sharpened pencil, 2 lined papers.
+    #You've completed your morning classes. It's time for lunch!
+
+#What would you like to eat?
+
+#Choices
+
+#A) Eat the food provided by Species: Human, cafeteria.
+#B) Use your water balloons to create a distraction and steal food from Species: Human, staff lounge.
+#C) Do not eat.
+
+#Outcomes
+#A) The food was poisoned! You feel dizzy and your vision blurs. (decreased HP, -1 oxygen fillter)
+#B) You sucessfully stole food from Species: Human, staff lounge. You feel energized! (-2 water balloons, -1 oxygen fillter)
+#C) You did not eat. You feel weak and your vision blurs. (decreased HP)
+
+#Consequences
+#A) You lost too much HP from the poison. Species: Human, found you passed out and used you in their research. Game Over!
+#B) You completed MISSION POISON and are ready for afternoon classes. (increased HP, if HP is already full then +1 oxygen fillter)
+#C) You lost a little HP from not eating. (-1 oxygen fillter)
+
+#Scene 4 : Friend or Foe
+
+#You wrote your findings: I have sucessful attended School. I must now make contact with Species: Human, students to complete my report.
+
+#Description: A Unifrom with unfamilar writings weaved into it.
+    #Your last assignment as come in: MISSION FRIEND OR FOE.
+    #You've completed your classes for the day. It's time to make contact with Species: Human, students.
+    #You have not been given any additional items.
+
+#A event is happening in the schoolyard.
+#Students are gathering around something interesting.
+#Several Species: Human Students, are lied down scattered around the schoolyard gassping for air.
+#A Species: Human Teacher, is approaching.
+
+#Who should you approach?
+
+#Choices
+#A) Approach the Species: Human Teacher.
+#B) Join the surrounding Species: Human Students.
+#C) Do not approach anyone and leave school immediately.
+#D) Approach a Species: Human, cafeteria worker.
+#E) Approach a Species: Human, school principal.
+
+#Outcomes
+#A) The Species: Human Teacher, asks you to move.
+# a. "I am here to help! What happened?"
+# b. Write your own response.
+# c. You quickly use your oxygen fillters to help the students breathe until the emergency services arrive. (increased HP)
+
+#B) You bystand the situation, unsure of what to do. The event unfolds without you.
+#C) You leave school immediately.
+#D) You return to the cafateria and spot a Species: Human, cafeteria worker, being suspicious.
+#E) The Species: Human, school principal, sees you in distress and follows you to the scene.
+
+#Consequences
+#A)
+# a. The Species: Human Teacher becomes fearful of your unworldly dialect. The authorites are called and you are acused of the student's POISONING. You've failed MISSION FRIEND OR FOE. Game Over!
+# b. You use 1 sharpened pencil and 1 lined paper to write your response. The Species: Human Teacher, understands your message and helps you reach the students. Your thanked for your help. The unconcious students lasted in time for help to arrive. When the authorites came, you feel a sense of accomplishment and take your leave before they can discover you. (increased HP, -3 oxygen fillter) You've COMPLETED MISSION FRIEND OR FOE. Game Complete!
+# b2. You do not have enough oxygen fillters to help the students.  (Returned to Outcomes with -1 oxygen filter in inventory and only a. choice remaining.)
+# c. The surrounding students are to crowded and won't let you through to the unconcious students. The Species: Human Teacher that approched is not much help alone. The unconcious students did not last in time for help to arrive. When the authorites came, you feel a sense of disappointment and take your leave before they can discover you. You've COMPLETED MISSION FRIEND OR FOE. Game Complete!
+
+#B) You've completed MISSION FRIEND OR FOE. Game Complete!
+#C) You've completed MISSION FRIEND OR FOE. Game Complete!
+#D) You have a chance to gather more information about the event.
+# a. Approach Species: Human, cafeteria worker.
+# b. Approach a Species: Human, student studying alone in the cafateria.
+# c. Approach a group of Species: Human, students playing together.
+# d. Approch no one and leave school immediately.
+
+#Special Event: Choices
+#a. The Species: Human, cafeteria worker, is grateful for your concern but says they are too busy to talk. They quickly walk away. (-1 oxygen fillter. Return to Consequence #D)
+#b. You use 1 sharpened pencil and 1 lined paper to question the student. The Species: Human, student studying alone, thanks you for your help and praises your quick thinking. They say the cafeteria staff was resently changed. (-1 oxygen fillter. Return to Consequence #D)
+#c. The group of Species: Human, students playing together in the cafateria, welcome you into their group and share their snacks with you.
+#d. It's none of by business.
+
+#Special Event: Outcomes
+#b. The student says they noticed the cafeteria staff acting strange recently, often sneaking around and whispering to themselves. They mention that the snacks provided by the cafeteria have been tasting off lately. They suggest investigating the cafeteria further.
+#c. The snacks were apart of the cafateria lunch!
+#d. You leave school immediately.
+
+#Special Event: Consequences
+#a. You've completed MISSION FRIEND OR FOE. Game Complete!
+#b. After additional investigation, you discover that the cafeteria worker was indeed poisoning the students as part of a attempt to "help". Confused, the cafteria worker ask if you would like to join MISSION SABATAGE. You refuse and anonmously report your findings to the authorities, who take swift action to ensure the safety of all students. You've completed MISSION FRIEND OR FOE. Game Complete!
+#c. Game Over! You've failed MISSION FRIEND OR FOE.
+#d. You've completed MISSION FRIEND OR FOE. Game Complete!
+
+
+#Congratulations Space Agent, you have sucessfully completed MISSION SCHOOL. Your species is one step closer to inhabiting Planet Earth. Your report will be sent back to your species headquarters for further analysis. Well done!
+
+#End of Game
+#Thank you for playing!
+
+#Goodbye Space Agent!
+#COMING SOON: MISSION SABATAGE
